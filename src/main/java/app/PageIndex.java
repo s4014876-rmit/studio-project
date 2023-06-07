2471430b58389545a101d48cad5e599599749950
Shane Knight <s3785357@student.rmit.edu.au>
2023-06-07 23:54:18 +1000

    PgIndxCSS creation+container arrangement+tab notes

--- src/main/java/app/PageIndex.java
+++ src/main/java/app/PageIndex.java
@@ -41,45 +41,84 @@ public class PageIndex implements Handler {
             </div>
         """;
         
-         // CREATE Tabs
+    
         html = html + """
-        <div class='tabs'>
-            <input type='radio' class='tabs__radio' name='regions' id='WorldTab' checked>
-            <label for='WorldTab' class='tabs__label'>World</label>
-                <div class='tabs__content'>
-                    <!--CONTENT for Tab World-->
-                        <h3>World</h3>
-                        <p>This is where all the <i>World</i> data will go:)</p>
-                </div>
-        
-        <!--CREATE Country Tab-->
-            <input type='radio' class='tabs__radio' name='regions' id='CountryTab'>
-            <label for='CountryTab' class='tabs__label'>Country</label>
-                <div class='tabs__content'>
-                    <!--CONTENT for Tab CountryTab-->
-                        <h3>Country</h3>
-                        <p>This is where all the <i>Country</i> data will go:)</p>
-                </div>
-        
-        <!--CREATE State Tab-->
-            <input type='radio' class='tabs__radio' name='regions' id='StateTab'>
-            <label for='StateTab' class='tabs__label'>State</label>
-                <div class='tabs__content'>
-                    <!--CONTENT for Tab StateTab-->
-                        <h3>State</h3>
-                        <p>This is where all the <i>State</i> data will go:)</p>
-                </div>
+        <link rel='stylesheet' type='text/css' href='PageIndexStyle.css'>
+        """;
 
-        <!--CREATE City Tab-->
-            <input type='radio' class='tabs__radio' name='regions' id='CityTab'>
-            <label for='CityTab' class='tabs__label'>City</label>
-                <div class='tabs__content'>
-                    <!--CONTENT for Tab CityTab-->
-                        <h3>City</h3>
-                        <p>This is where all the <i>City</i> data will go:)</p>
+    //Max container - overall container
+    html = html + """
+    <div class='Max_Container'>
+
+
+        <!-- Data Display -->
+
+        <div class='Disp_Container'>
+            <div class='World_Image'>
+                <h3>Picture of world i guess</h3>
+                <p>not really sure tho</p>
+            </div>
+
+            <div class='Data_Ranges'>
+                <h4>AvgGlobalTemp</h4>
+                <p>|------------------|XXX - XXX</p>
+                <br></br>
+                <h4>LandOceanAvgTemp</h4>
+                <p>|------------------|XXX - XXX</p>
+                <br></br>
+                <h4>TotalPopulation</h4>
+                <p>|------------------|XXX - XXX</p>
+                <br></br>
+                <h4>TotalPopulation</h4>
+                <p>|------------------|XXX - XXX</p>
+                <br></br>
+            </div>
+        </div>
+        
+        <!-- Tabs & content -->
+
+        <div class='Tabs_Container'>
+            <div class='tabs'>
+            <!--CREATE Country Tab-->
+                <input type='radio' class='tabs__radio' name='regions' id='WorldTab' checked>
+                <label for='WorldTab' class='tabs__label'>World</label>
+                    <div class='tabs__content'>
+                        <!--CONTENT for Tab World-->
+                            <h3>World</h3>
+                            <p>This is where all the <i>World</i> data will go:)</p>
+                    </div>
+            
+            <!--CREATE Country Tab-->
+                <input type='radio' class='tabs__radio' name='regions' id='CountryTab'>
+                <label for='CountryTab' class='tabs__label'>Country</label>
+                    <div class='tabs__content'>
+                        <!--CONTENT for Tab CountryTab-->
+                            <h3>Country</h3>
+                            <p>This is where all the <i>Country</i> data will go:)</p>
+                    </div>
+            
+            <!--CREATE State Tab-->
+                <input type='radio' class='tabs__radio' name='regions' id='StateTab'>
+                <label for='StateTab' class='tabs__label'>State</label>
+                    <div class='tabs__content'>
+                        <!--CONTENT for Tab StateTab-->
+                            <h3>State</h3>
+                            <p>This is where all the <i>State</i> data will go:)</p>
+                    </div>
+
+            <!--CREATE City Tab-->
+                <input type='radio' class='tabs__radio' name='regions' id='CityTab'>
+                <label for='CityTab' class='tabs__label'>City</label>
+                    <div class='tabs__content'>
+                        <!--CONTENT for Tab CityTab-->
+                            <h3>City</h3>
+                            <p>This is where all the <i>City</i> data will go:)</p>
+                    </div>
                 </div>
             </div>
-        """;
+        </div>
+    </div>
+    """;
 
         html += CommonElements.Footer();
         html += CommonElements.DocumentEnd();
